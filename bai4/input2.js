94f96d546c495a5e1f432f32793bd0583c6ff378
thangnguyen2005 <nguyenngocthang081220052gmail.com>
2023-09-19 10:58:21 +0700

    baitap1

---
+++ bai4/input2.js
@@ -0,0 +1,7 @@
+
+function showMessage() {
+    alert('xin chào!')
+    document.getElementById('messageDiv').innerHTML = 'xin chào!';
+    document.write('xin chào!');
+    cosole.log('xin chào');
+}

77ab44faced83e203cd82e1a58635872aa81356a
thangnguyen2005 <nguyenngocthang081220052gmail.com>
2023-09-20 10:37:51 +0700

    bai6.1

---
+++ bai4/bai5/bai6.js
@@ -0,0 +1,9 @@
+var inputWidth;
+var inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+document.write("The area is" + area);
+
